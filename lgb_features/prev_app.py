48510d0ab25ced845d6962f17492468d9fff7e9a
Andy Song <congxisong@hotmail.com>
2026-01-13 11:40:30 +0000

    fetures done related to amt

--- lgb_features/prev_app.py
+++ lgb_features/prev_app.py
@@ -2,6 +2,29 @@ import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from scipy.optimize import fsolve
+
+def solve_interest_rate(P, A, n):
+    if P <= 0 or A <= 0 or n <= 0:
+        return np.nan
+    
+    def f(r):
+        return P * (r * (1 + r)**n) / ((1 + r)**n - 1) - A
+    
+    try:
+        r = fsolve(f, 0.03)[0]
+        return r
+    except:
+        return np.nan
+
+def aggregation(df, col):
+    columns = ['SK_ID_CURR', col]
+    d_0 = df[df['seq']==0][columns].set_index('SK_ID_CURR')
+    d_3 = df[df['seq']<3].groupby(['SK_ID_CURR'])[col].agg(['min', 'max', 'mean', 'median','std'])
+    d = df.groupby(['SK_ID_CURR'])[col].agg(['min', 'max', 'mean', 'median','std'])
+    d = pd.concat([d_0, d_3, d], axis=1)
+    return d
 
 def build_features(df):
     active_status = df.groupby('SK_ID_CURR')['NAME_CONTRACT_TYPE'].count()
@@ -14,14 +37,17 @@ def build_features(df):
     contract_status = df.groupby(['SK_ID_CURR', 'NAME_CONTRACT_STATUS'])['ref'].sum().unstack().fillna(0)
     contract_status = contract_status / contract_status.sum(axis=1)[:, None]
     
-    app_credit_0 = df[df['seq']==0][['SK_ID_CURR','amt_credit_diff', 'amt_credit_ratio']].set_index('SK_ID_CURR')
-    app_credit_3 = df[df['seq']<3].groupby(['SK_ID_CURR'])['amt_credit_diff'].agg(['min', 'max', 'mean', 'median','std'])
-    app_credit = df.groupby(['SK_ID_CURR'])['amt_credit_diff'].agg(['min', 'max', 'mean', 'median','std'])
-    app_credit_ratio_3 = df[df['seq']<3].groupby(['SK_ID_CURR'])['amt_credit_ratio'].agg(['min', 'max', 'mean', 'median','std'])
-    app_credit_ratio = df.groupby(['SK_ID_CURR'])['amt_credit_ratio'].agg(['min', 'max', 'mean', 'median','std'])
+    app_credit = aggregation(df, 'amt_credit_diff')
+    app_credit_ratio = aggregation(df, 'amt_credit_ratio')
+    interest_portion = aggregation(df, 'interest_ratio')
+    interest_rate = aggregation(df, 'interest_rate')
+    down_payment_ratio = aggregation(df, 'down_payment_ratio')
+    annuity_income_ratio = aggregation(df, 'annuity_income_ratio')
+    credit_income_ratio = aggregation(df, 'credit_income_ratio')
     
     x = pd.concat([active_status, active_days_per_loan, last_loan, contract_status_0, contract_status_3, contract_status,
-                   app_credit_0, app_credit_3, app_credit, app_credit_ratio_3, app_credit_ratio], axis=1)
+                   app_credit, app_credit_ratio, interest_portion, interest_rate, down_payment_ratio, annuity_income_ratio,
+                   credit_income_ratio], axis=1)
     return x
 
 if __name__ == '__main__':
@@ -40,6 +66,21 @@ if __name__ == '__main__':
     prev_app['amt_credit_diff'] = prev_app['AMT_CREDIT'] - prev_app['AMT_APPLICATION']
     prev_app['amt_credit_ratio'] = prev_app['amt_credit_diff'] / prev_app['AMT_APPLICATION']
     prev_app['amt_credit_diff'] = np.log1p(prev_app['amt_credit_diff'])
+    
+    prev_app['interest_ratio'] = (prev_app['AMT_ANNUITY'] * prev_app['CNT_PAYMENT'] - prev_app['AMT_CREDIT']) / prev_app['AMT_CREDIT']
+    prev_app.loc[prev_app['CNT_PAYMENT']==0, 'interest_ratio'] = np.nan
+    prev_app['down_payment_ratio'] = prev_app['AMT_DOWN_PAYMENT'] / prev_app['AMT_GOODS_PRICE']
+    
+    interest_rates = []
+    z = prev_app[['AMT_ANNUITY', 'AMT_CREDIT', 'CNT_PAYMENT']].to_numpy()
+    for P, A, n in tqdm(z, total=z.shape[0], desc='calculating interest rate'):
+        interest_rates.append(solve_interest_rate(P, A, n))
+    prev_app['interest_rate'] = interest_rates
+    prev_app.loc[prev_app['CNT_PAYMENT']==0, 'interest_rate'] = np.nan
+    
+    prev_app = prev_app.merge(train[['SK_ID_CURR', 'AMT_INCOME_TOTAL']], how='left', on=['SK_ID_CURR'])
+    prev_app['annuity_income_ratio'] = prev_app['AMT_ANNUITY'] / prev_app['AMT_INCOME_TOTAL']
+    prev_app['credit_income_ratio'] = prev_app['AMT_CREDIT'] / prev_app['AMT_INCOME_TOTAL']
     
     x = build_features(prev_app)
     val_currs = np.load('artifacts/val_sk_currs.npy')
@@ -63,30 +104,15 @@ if __name__ == '__main__':
 # 32696.1 * 48
 
 
-
+prev_app['interest_ratio']
 
 
 
 #%%
-# from tqdm import tqdm
-# from scipy.optimize import fsolve
-# def solve_interest_rate(P, A, n):
-#     if P <= 0 or A <= 0 or n <= 0:
-#         return np.nan
+
+
+
     
-#     def f(r):
-#         return P * (r * (1 + r)**n) / ((1 + r)**n - 1) - A
-    
-#     try:
-#         r = fsolve(f, 0.03)[0]
-#         return r
-#     except:
-#         return np.nan
-# interest_rates = []
-# z = prev_app[~(prev_app["AMT_ANNUITY"] * prev_app["CNT_PAYMENT"] < prev_app["AMT_CREDIT"])]
-# z = z[['AMT_ANNUITY', 'AMT_CREDIT', 'CNT_PAYMENT']].to_numpy()
-# for P, A, n in tqdm(z, total=z.shape[0], desc='calculating interest rate'):
-#     interest_rates.append(solve_interest_rate(P, A, n))
 
 
 
